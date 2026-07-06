1ce5b9aaf0d8b22c651b83667ecb9be57784f84a
Javier <javier.morenof@um.es>
2026-07-07 00:49:44 +0200

    Añadidos controles de camara para que siga al cubo. Usado Delta Time y grabedad.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <iostream>
 
 // Constantes de Configuración de la Ventana
 #define SCREEN_WIDTH 900  // Ancho de la ventana en píxeles
@@ -13,15 +14,18 @@
 #define INITIAL_CAMERA_FOVY 45.0f                           // Amplitud del lente (el ángulo de apertura vertical)
 
 // Constantes del Entorno y Objetos
-#define GRID_SLICES 20                            // Número de divisiones en la cuadrícula
-#define GRID_SPACING 1.0f                         // Espaciado entre las divisiones de la cuadrícula
-#define CUBE_POSITION (Vector3){0.0f, 1.0f, 0.0f} // Posición del cubo central en el mundo
-#define CUBE_SIZE 1.0f                            // Tamaño del cubo (Ancho, Alto, Largo)
+#define GRID_SLICES 20                                                         // Número de divisiones en la cuadrícula
+#define GRID_SPACING 1.0f                                                      // Espaciado entre las divisiones de la cuadrícula
+#define CUBE_POSITION (Vector3){-CUBE_SIZE / 2, CUBE_SIZE / 2, -CUBE_SIZE / 2} // Posición del cubo central en el mundo
+#define CUBE_SIZE 1.0f                                                         // Tamaño del cubo (Ancho, Alto, Largo)
 
 // Constantes de la Interfaz (UI)
 #define TEXT_POS_X 10     // Posición X del texto en la pantalla
 #define TEXT_POS_Y 10     // Posición Y del texto en la pantalla
 #define TEXT_FONT_SIZE 20 // Tamaño de la fuente del texto
+
+// Actualiza la posición de la cámara con respecto a la posición de un objeto, de forma suave.
+void actualizarPosicionCamara(Vector3 posicionObjeto, Camera3D &camera);
 
 int main()
 {
@@ -39,7 +43,14 @@ int main()
     // Establece el objetivo de fotogramas por segundo de la ventana
     SetTargetFPS(MAX_FPS);
 
-    // Objetos del enterno
+    // variables Fisicas
+
+    float const gravedad = -14.0f; // Casillas por segundo
+    float velocidad = 10.0f;       // Casillas por segundo
+    float fuerzaSalto = 9.0f;      // Casillas por segundo
+    float velocidadY = 0.0f;       // velocidad de ascenso/caida
+
+    // Objetos del entorno
     Vector3 posicionCubo = CUBE_POSITION;
 
     // TIPO_VARIABLE NOMBRE;
@@ -47,36 +58,52 @@ int main()
 
     while (!WindowShouldClose())
     {
+        float deltaTime = GetFrameTime(); // Guardamos el tiempo del frame para mayor claridad
         // =========================================================================
         // 1. SECCIÓN DE ENTRADA (Capturar lo que hace el usuario)
         // =========================================================================
         if (IsKeyDown(KEY_A))
         {
-            posicionCubo = Vector3Add(posicionCubo, {-1.0f, 0.0f, 0.0f}); // Posicion actual, la desplazamos 1 punto a la izquierda.
+            posicionCubo.x += -1.0f * velocidad * deltaTime; // Posicion actual, la desplazamos 1 punto a la izquierda.
         }
         if (IsKeyDown(KEY_D))
         {
-            posicionCubo = Vector3Add(posicionCubo, {1.0f, 0.0f, 0.0f}); // Posicion actual, la desplazamos 1 punto a la derecha.
+            posicionCubo.x += +1.0f * velocidad * deltaTime; // Posicion actual, la desplazamos 1 punto a la derecha.
         }
 
         if (IsKeyDown(KEY_W))
         {
-            posicionCubo = Vector3Add(posicionCubo, {0.0f, 0.0f, -1.0f}); // Posicion actual, la desplazamos 1 punto hacia arriba.
+            posicionCubo.z += -1.0f * velocidad * deltaTime; // Posicion actual, la desplazamos 1 punto hacia arriba.
         }
 
         if (IsKeyDown(KEY_S))
         {
-            posicionCubo = Vector3Add(posicionCubo, {0.0f, 0.0f, 1.0f}); // Posicion actual, la desplazamos 1 punto hacia abajo.
+            posicionCubo.z += 1.0f * velocidad * deltaTime; // Posicion actual, la desplazamos 1 punto hacia abajo.
         }
 
         if (IsKeyPressed(KEY_SPACE))
         {
-            posicionCubo = Vector3Add(posicionCubo, {0.0f, 1.0f, 0.0f}); // Posicion actual, la desplazamos 1 punto hacia arriba.
+            velocidadY = fuerzaSalto; // Posicion actual, la desplazamos 1 punto hacia arriba.
         }
 
         // =========================================================================
         // 2. SECCIÓN DE ACTUALIZACIÓN (Cálculos, físicas y lógica)
         // =========================================================================
+
+        // 1. Aplicamos la gravedad a la velocidad en y de forma constante
+        velocidadY += gravedad * deltaTime;
+
+        // 2. Movemos el cubo según la velocidad resultante
+        posicionCubo.y += velocidadY * deltaTime;
+
+        // 3. Control de colisión con el suelo (si baja de la altura del suelo, lo frena)
+        if (posicionCubo.y < 0.5f)
+        {
+            posicionCubo.y = 0.5f; // Lo posiciona exactamente sobre el suelo
+            velocidadY = 0.0f;     // Cancela la fuerza de caída
+        }
+
+        actualizarPosicionCamara(posicionCubo, camera);
 
         // =========================================================================
         // 3. SECCIÓN DE RENDERIZADO (Dibujar todo en pantalla)
@@ -89,7 +116,6 @@ int main()
         BeginMode3D(camera);
         // Dibuja el cubo utilizando las constantes definidas
         DrawCube(posicionCubo, CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, RED);
-
         // X, Y, Z ()
 
         // Dibuja la cuadrícula de guía en el suelo
@@ -106,4 +132,16 @@ int main()
 
     // Cierra la ventana y libera los recursos
     CloseWindow();
+}
+
+void actualizarPosicionCamara(Vector3 posicionObjeto, Camera3D &camera)
+{
+    Vector3 camaraDistancia = INITIAL_CAMERA_POSITION;
+    float suavizadoCamara = 8.0f;
+
+    Vector3 posicionIdealCamara = Vector3Add(posicionObjeto, camaraDistancia);
+    Vector3 objetivoIdealCamara = posicionObjeto;
+
+    camera.position = Vector3Lerp(camera.position, posicionIdealCamara, suavizadoCamara * GetFrameTime());
+    camera.target = Vector3Lerp(camera.target, objetivoIdealCamara, suavizadoCamara * GetFrameTime());
 }
